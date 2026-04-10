11711d70de4b9a66da9944125fb676e56bf70c8e
doutyacodes <doutyacode@gmail.com>
2026-04-10 08:39:25 +0530

    dynamic changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wowfy.in",
+        port: "",
+        pathname: "/wowfy_app_codebase/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
